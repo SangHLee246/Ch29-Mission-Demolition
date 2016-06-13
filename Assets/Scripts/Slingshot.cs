11870e64f9377aadfda9899531d48276f0fd1b42
SangHLee246 <akayasha1234@yahoo.com>
2016-06-13 18:02:37 -0400

    Projectile Launches
    Added C# code, so the projectile now launches.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	public bool __________________________;
+
 	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
 
 	void Awake(){
 		Transform launchPointTrans = transform.Find ("LaunchPoint");
 		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
 	}
 
 	void OnMouseEnter(){
@@ -20,6 +28,12 @@ public class Slingshot : MonoBehaviour {
 		launchPoint.SetActive (false);
 	}
 
+	void OnMouseDown() {
+		aimingMode = true;
+		projectile = Instantiate (prefabProjectile) as GameObject;
+		projectile.transform.position = launchPos;
+		projectile.rigidbody.isKinematic = true;
+	}
 	// Use this for initialization
 	void Start () {
 	
@@ -27,6 +41,25 @@ public class Slingshot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!aimingMode)
+			return;
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		Vector3 mouseDelta = mousePos3D - launchPos;
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize ();
+			mouseDelta *= maxMagnitude;
+		}
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp (0)) {
+			aimingMode = false;
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			projectile = null;
+		}
 	}
 }
